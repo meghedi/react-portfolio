af3b7f12ce7331123a42f2b5bd0c1ebc8eb6f9c9
meghedi <hacopian.m@gmail.com>
2020-08-04 00:05:46 -0700

    fixed typo issues

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -9,16 +9,16 @@ function About() {
         <div className="content">
           Meghedi loves programming,
           <p>
-            She has almost 10 years of hands-on experience efficiently coding
+            She has almost 7 years of hands-on experience efficiently coding
             websites and applications as a Software Engineer.
           </p>
           <p>
             Building easy to use, user-friendly websites and applications is
-            truly a passion of hers. In addition to my knowledge base, she
-            actively seek out new technologies and stay up-to-date on industry
+            truly a passion of hers. In addition to her knowledge base, she
+            actively seeks out new technologies and stays up-to-date on industry
             trends and advancements. This has allowed her to stay ahead of the
-            curve and deliver exceptional work to all of my employers, including
-            those I've worked for on a project basis.
+            curve and deliver exceptional work to all of her employers, including
+            those she has worked for on a project basis.
           </p>
           <div className="btnsDiv">
             <a
